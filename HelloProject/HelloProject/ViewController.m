20a25127a6ccdb4d6f072231c6909ab444d50eee
赵凯 <ZKai@zhaokaitekiiMac.local>
2015-04-30 19:08:14 +0800

    111

---
+++ HelloProject/HelloProject/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HelloProject
+//
+//  Created by 赵凯 on 15/4/30.
+//  Copyright (c) 2015年 zhaokai. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
